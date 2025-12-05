84259fa1381b563ed6333664bf462c4a7f728552
DituIulian <dituiulian@gmail.com>
2025-12-05 14:41:58 +0200

    05.12.2025

--- Switch/src/Main.java
+++ Switch/src/Main.java
@@ -4,21 +4,20 @@ public class Main {
 
         int switchValue = 4;
 
-        switch (switchValue){
-            case 1:
-                System.out.println("Value was 1");
-                break;
-            case 2:
-                System.out.println("Value was 2");
-                break;
-            case 3: case 4: case 5:
+        switch (switchValue) {
+            case 1 -> System.out.println("Value was 1");
+            case 2 -> System.out.println("Value was 2");
+            case 3, 4, 5 -> {
                 System.out.println("Was a 3, a 4, or a 5");
                 System.out.println("Actually it was a " + switchValue);
-                break;
-            default:
-                System.out.println("Value was not 1, 2, 3, 4 or 5");
+            }
+            default -> System.out.println("Value was not 1, 2, 3, 4 or 5");
         }
-
-
     }
+
+
+     public static String getQuarter(String month){
+
+        return "";
+     }
 }
