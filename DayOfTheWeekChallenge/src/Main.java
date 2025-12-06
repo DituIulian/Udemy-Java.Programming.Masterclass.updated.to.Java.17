56019bb42a0a409489140bf9f4769507d46a1f6c
DituIulian <dituiulian@gmail.com>
2025-12-06 17:35:08 +0200

    06.12.2025

---
+++ DayOfTheWeekChallenge/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        printDayOfWeek(2);
+    }
+
+    public static void printDayOfWeek(int dayNumber){
+
+        switch (dayNumber){
+            case 0 -> System.out.println("Sunday");
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+
+            default -> System.out.println("Invalid day");
+        };
+    }
+
+}
