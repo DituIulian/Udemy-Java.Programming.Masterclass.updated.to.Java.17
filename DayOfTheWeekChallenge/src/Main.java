4613a6d8de1095e2251d29d33709d0ca308ef73a
DituIulian <dituiulian@gmail.com>
2025-12-07 19:34:50 +0200

    07.12.2025

--- DayOfTheWeekChallenge/src/Main.java
+++ DayOfTheWeekChallenge/src/Main.java
@@ -2,22 +2,63 @@ public class Main {
 
     public static void main(String[] args) {
 
-        printDayOfWeek(2);
-    }
+       int weekDay = 2;
 
-    public static void printDayOfWeek(int dayNumber){
+       String dayOfTheWeek =  switch (weekDay){
+            case 0 -> ("Sunday");
+            case 1 -> ("Monday");
+            case 2 -> ("Tuesday");
+            case 3 -> ("Wednesday");
+            case 4 -> ("Thursday");
+            case 5 -> ("Friday");
+            case 6 -> ("Saturday");
 
-        switch (dayNumber){
-            case 0 -> System.out.println("Sunday");
-            case 1 -> System.out.println("Monday");
-            case 2 -> System.out.println("Tuesday");
-            case 3 -> System.out.println("Wednesday");
-            case 4 -> System.out.println("Thursday");
-            case 5 -> System.out.println("Friday");
-            case 6 -> System.out.println("Saturday");
-
-            default -> System.out.println("Invalid day");
+            default -> ("Invalid day");
         };
+
+        System.out.println("day " + weekDay + " is for " + dayOfTheWeek);
+
+        int day = 6;
+        String weekDayIf =  printWeekDay(day);
+        System.out.println("day " + day + " is for " + weekDayIf);
+
     }
 
+//    public static void printDayOfWeek(int dayNumber){
+//
+//        switch (dayNumber){
+//            case 0 -> System.out.println("Sunday");
+//            case 1 -> System.out.println("Monday");
+//            case 2 -> System.out.println("Tuesday");
+//            case 3 -> System.out.println("Wednesday");
+//            case 4 -> System.out.println("Thursday");
+//            case 5 -> System.out.println("Friday");
+//            case 6 -> System.out.println("Saturday");
+//
+//            default -> System.out.println("Invalid day");
+//        }
+//    }
+
+
+    public static String printWeekDay(int weekDay) {
+
+        if (weekDay == 0) {
+            return "Sunday";
+        } else if (weekDay == 1) {
+            return "Monday";
+        } else if (weekDay == 2) {
+            return "Tuesday";
+        } else if (weekDay == 3) {
+            return "Wednesday";
+        } else if (weekDay == 4) {
+            return "Thursday";
+        } else if (weekDay == 5) {
+            return "Friday";
+        } else if (weekDay == 6) {
+            return "Saturday";
+        } else {
+            return "Invalid day";
+        }
+
+    }
 }
