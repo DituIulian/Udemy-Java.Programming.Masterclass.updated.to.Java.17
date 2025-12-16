3ab9207531a8da85eceea65e209a2aaeb09c089a
DituIulian <dituiulian@gmail.com>
2025-12-16 17:51:17 +0200

    16.12.2025

--- DigitSumChallenge/src/Main.java
+++ DigitSumChallenge/src/Main.java
@@ -5,29 +5,30 @@ public class Main {
 
         System.out.println(sumDigits(-22));
         System.out.println(sumDigits(7));
-        System.out.println(sumDigits(372));
+        System.out.println(sumDigits(4234));
+        System.out.println(sumDigits(12134));
 
     }
 
-    public static int sumDigits(int digit){
-    // nu trebuie sa accepte un numar negativ - daca este negativ trb sa returneze -1 ( care inseamna invalid value)
-        // sum se face pt digiti - 125 e 1 + 2 + 5 deci 8
-        //daca este un numar de 1 digit, returneaza numarul
-        if (digit < 0){
+    public static int sumDigits(int number){
+
+        if (number < 0){
             return  -1;
         }
 
-        if (digit >= 10){
-            int sum = 0;
-            while ( sum < 1){
-              sum = digit % 10;
-                
+        int sum = 0;
+
+        while (number > 9){
+              sum += number % 10;
+              number = number / 10;
             }
 
-            return sum;
+        sum += number;
+
+        return sum;
+
         }
 
-        return digit;
-    }
+
 
 }
